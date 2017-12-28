8c2c194af14454f260b74770c2d2f5563c15fa68
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-28 11:15:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  Object.assign({}, object, {[key], value})
+  delete object[key];
+  return object;  
+}
